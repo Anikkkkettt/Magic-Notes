2c50de3269ebe49dc9b7eb01500986fa3c4a031e
Aniket Nayek <nayekaniket2017@gmail.com>
2022-02-06 18:24:32 +0530

    Adding JS

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
 let addBtn = document.getElementById('addBtn');
-let addTxt = document.getElementById('addTxt');
 
-addBtn.addEventListener("click",function(){
-    console.log("Clicked");
-    html = "";
-    i = 0;
-    html += `<div class="card" style="width: 18rem;">
-    <div class="card-body">
-      <h5 class="card-title">Note ${++i}</h5>
-      <p class="card-text">${addTxt.value}</p>
-      <button  class="btn btn-primary">Delete Note</button>
-    </div>
-  </div>`
-  document.getElementById('notes').innerHTML = html;
-  addTxt.value="";
+addBtn.addEventListener("click", function (e) {
+  let addTxt = document.getElementById('addTxt');
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  }
+  else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.push(addTxt.value);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  addTxt.value = "";
+  shownotes();
 })
+
+function shownotes() {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  }
+  else {
+    notesObj = JSON.parse(notes);
+  }
+  let html = "";
+  notesObj.forEach(function (element, index) {
+    html += `
+    <div class="card my-2 mx-2" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">${index+1}</h5>
+        <p class = "card-text">${element}</p>
+        <button class="btn btn-primary" >Delete note</button>
+    </div>
+</div>`
+  });
+  let notesElm = document.getElementById('notes');
+  if(notesObj.length != 0){
+    notesElm.innerHTML = html;
+  }
+  else{
+    notesElm.innerHTML = `No notes to show! Use "Add a notes" section to add your note`
+  }
+}
