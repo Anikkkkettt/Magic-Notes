818ab505bc7702834a649673768cbd32ea39d9a4
Aniket Nayek <nayekaniket2017@gmail.com>
2022-02-07 20:43:22 +0530

    Adding search functionality

--- script.js
+++ script.js
@@ -27,9 +27,9 @@ function shownotes() {
   let html = "";
   notesObj.forEach(function (element, index) {
     html += `
-    <div class="card my-2 mx-2" style="width: 18rem;">
+    <div class="noteCard card my-2 mx-2" style="width: 18rem;">
       <div class="card-body">
-        <h5 class="card-title">${index+1}</h5>
+        <h5 class="card-title">Note ${index+1}</h5>
         <p class = "card-text">${element}</p>
         <button id="${index}" onclick="deleteNotes(this.id)" class="btn btn-primary" >Delete note</button>
     </div>
@@ -57,3 +57,18 @@ function deleteNotes(index){
   localStorage.setItem("notes", JSON.stringify(notesObj));
   shownotes();
 }
+
+let search = document.getElementById('searchTxt');
+search.addEventListener("input",function(){
+  let inputVal = search.value.toLowerCase();
+  let notesCard = document.getElementsByClassName('noteCard');
+  Array.from(notesCard).forEach(function(element){
+    let cardTxt = element.getElementsByTagName("p")[0].innerText;
+    if(cardTxt.includes(inputVal)){
+      element.style.display = "block";
+    }
+    else{
+      element.style.display = "none";
+    }
+  })
+})
